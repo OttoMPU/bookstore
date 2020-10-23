6f6114c6c65974328c14ae20b0684ce95af3eca7
otto puistola <otto.puistola@gmail.com>
2020-10-23 13:18:08 +0300

    deployment

--- src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/swd20/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import hh.swd20.Bookstore.domain.Book;
 import hh.swd20.Bookstore.domain.BookRepository;
 import hh.swd20.Bookstore.domain.Category;
 import hh.swd20.Bookstore.domain.CategoryRepository;
+import hh.swd20.Bookstore.domain.User;
+import hh.swd20.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +20,7 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository) {
+	public CommandLineRunner demo(BookRepository brepository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 			Category c1 = new Category("Fantasy");
@@ -34,6 +36,12 @@ public class BookstoreApplication {
 			
 			brepository.save(new Book("Turms kuolematon", "Mika Waltari", "123-0-20750-0", 1956, c1));
 			brepository.save(new Book("Crime And Punishment", "Fyodor Dostoyevsky", "0143058142", 1866, c4));
+			
+			User user1 = new User("user", "$2a$04$evu5Qu.514/TiM9MPx9rcuN0WQ7mubbirrXqDkbOvEM5hS/j9/Wj2", "USER");
+			User user2 = new User("admin", "$2a$04$895bRHQd0/YH2yoN8IBRUeDOYL298gvjMDihWx/xnqDP.kSJPBDiW", "ADMIN");
+			
+			urepository.save(user1);
+			urepository.save(user2);
 			
 		};
 	}
